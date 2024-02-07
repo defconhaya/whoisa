b9aeb28011ef0774796d9bfb5e63aed10ae89d00
Haya <razvan.olteanu@nortonlifelock.com>
2024-02-07 18:23:47 +0200

    rewrite whois client

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use clap::Parser;
+use indicatif::{ProgressBar, ProgressStyle};
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 use std::collections::BTreeMap;
+use std::fs;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::sync::Arc;
 use tokio::sync::Semaphore;
-use whois_rust::{WhoIs, WhoIsLookupOptions};
-use indicatif::{ProgressBar, ProgressStyle};
-use std::fs;
 
+mod modules;
+use modules::whois;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct WhoisInfo {
@@ -56,18 +57,20 @@ fn parse_whois_to_json(whois: &str) -> WhoisInfo {
     whois_map
 }
 
-async fn ip(semaphore: Arc<Semaphore>, ip: String, progress_bar: Arc<ProgressBar>) -> WhoisInfo {
+async fn ip(semaphore: Arc<Semaphore>, ip: String, progress_bar: Arc<ProgressBar>) -> Result< WhoisInfo, String> {
     let json = whoiser(semaphore, ip, progress_bar).await;
     json
 }
 
-async fn whoiser(semaphore: Arc<Semaphore>, ip: String, progress_bar: Arc<ProgressBar>) -> WhoisInfo {
+async fn whoiser(
+    semaphore: Arc<Semaphore>,
+    ip: String,
+    progress_bar: Arc<ProgressBar>,
+) ->Result< WhoisInfo, String>
+ {
     // progress_bar.println(format!("\n{} is being served by the teller", ip));
     let permit = semaphore.acquire().await.unwrap();
-    let whois = WhoIs::from_path("servers.json").unwrap();
-    let result: String = whois
-        .lookup(WhoIsLookupOptions::from_string(ip.clone()).unwrap())
-        .unwrap();
+    let result = whois("193.0.6.135:43", &ip);
     let mut json_result = parse_whois_to_json(&result);
     json_result
         .other_fields
@@ -76,7 +79,7 @@ async fn whoiser(semaphore: Arc<Semaphore>, ip: String, progress_bar: Arc<Progre
     progress_bar.inc(1);
     progress_bar.set_message(ip);
     drop(permit);
-    json_result
+    return Ok(json_result);
 }
 
 fn get_value<'a>(map: &'a BTreeMap<String, String>, keys_to_try: Vec<&'a str>) -> &'a str {
@@ -91,15 +94,8 @@ fn get_value<'a>(map: &'a BTreeMap<String, String>, keys_to_try: Vec<&'a str>) -
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-    // let mut parts: Vec<Vec<&str>> = Vec::<Vec<&str>>::new();
-    // let binding = args.capture.to_lowercase();
-    // let captures: Vec<&str> = binding.split(',').collect();
-    // for alt in &captures {
-    //     let alt_vec: Vec<&str> = alt.split('|').collect();
-    //     parts.push(alt_vec);
-    // }
 
-    
+
     match File::open(&args.file) {
         Ok(file_handle) => {
             let reader = io::BufReader::new(file_handle);
@@ -108,21 +104,28 @@ async fn main() {
             let semaphore = Semaphore::new(num_of_tellers.try_into().unwrap());
             let semaphore_arc = Arc::new(semaphore);
 
+            let line_count = fs::read_to_string(args.file)
+                .expect("Error reading file")
+                .lines()
+                .count()
+                .try_into()
+                .unwrap();
 
-            let line_count= fs::read_to_string(args.file).expect("Error reading file").lines().count().try_into().unwrap();
-
-
-            let progress_bar = Arc::new(ProgressBar::new(line_count));            
-                progress_bar.set_style(
+            let progress_bar = Arc::new(ProgressBar::new(line_count));
+            progress_bar.set_style(
                     ProgressStyle::with_template("[{bar:40.green}] {pos}/{len} {percent}% {elapsed_precise}/{eta_precise} \t\t[{msg:.blue}]").expect("REASON")
                         .progress_chars("█▇▆▅▄▃▂▁  "));
-        
 
             for line in reader.lines() {
                 match line {
-                    Ok(line_content) => {                        
+                    Ok(line_content) => {
+                        // println!("{}", line_content);
                         let progress_bar_clone = progress_bar.clone();
-                        people_handlers.push(tokio::spawn(ip(semaphore_arc.clone(), line_content.trim().to_string(), progress_bar_clone )))
+                        people_handlers.push(tokio::spawn(ip(
+                            semaphore_arc.clone(),
+                            line_content.trim().to_string(),
+                            progress_bar_clone,
+                        )))
                     }
                     Err(_e) => {}
                 }
@@ -130,12 +133,13 @@ async fn main() {
 
             let mut results = Vec::new();
             for handle in people_handlers {
-                results.push(handle.await.unwrap());                
+                results.push(handle.await.unwrap());
             }
 
             let mut json_array: Vec<Value> = Vec::new();
-            for res in results {
-                let keys_to_try = vec!["org-name", "organization", "orgname"];
+            for result in results {
+                let keys_to_try = vec!["org-name", "organization", "orgname", "netname"];
+                let res = result.unwrap();
                 let item = json!({
                     res.other_fields.get("ip").unwrap(): json!( {"country":res.other_fields.get("country").unwrap_or(&"unk-country".to_string()),
                 "name":get_value(&res.other_fields, keys_to_try),})
---
+++ src/modules/mod.rs
@@ -0,0 +1,17 @@
+use std::io::Read;
+use std::time::Duration;
+use std::{io::Write, net::TcpStream};
+
+
+pub fn whois(server_addr:&str, ip:&str)->String{
+    let query = format!("-B {}\r\n", ip);
+    let mut client =  TcpStream::connect(server_addr).unwrap();
+    let _=client.set_read_timeout(Some(Duration::from_secs(1)));
+    let _=client.set_write_timeout(Some(Duration::from_secs(1)));
+    let _=client.write_all(query.as_bytes());
+    let _=client.flush();
+    let mut buffer = Vec::new();
+    let _response = client.read_to_end(&mut buffer);
+    let query_result = String::from_utf8_lossy(&buffer);
+    query_result.to_string()
+    }
